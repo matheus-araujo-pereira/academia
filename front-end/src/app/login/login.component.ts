03dee7b197d3c1715425768cf67d8c7cbd9005fb
Matheus Araujo Pereira <matheusaraujopereira@outlook.com>
2025-03-22 23:59:58 -0300

    Reapply "Reapply "feat: refatorar componente de login para usar Angular Material""
    This reverts commit 8cd77b56eb37a8702a833f12be42ff1eacb11cbb.

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -11,11 +11,24 @@ import {
 import { CommonModule } from '@angular/common';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
