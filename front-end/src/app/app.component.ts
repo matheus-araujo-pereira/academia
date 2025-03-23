a38074b635cb4a1c7772a61f14866a4b3994039f
Matheus Araujo Pereira <matheusaraujopereira@outlook.com>
2025-03-23 00:19:19 -0300

    feat: substituir menu lateral por menu fixo responsivo e remover componente de menu

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,25 +2,34 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { MenuComponent } from './menu/menu.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true, // componente standalone
-  imports: [RouterOutlet, CommonModule, MenuComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   showMenu = false;
+  isMobileMenuOpen = false;
 
   constructor(private router: Router) {
     // Exibe o menu se a rota não for "/login"
     this.router.events.subscribe((event) => {
-      // Implemente sua lógica para definir showMenu conforme a rota
-      // Por exemplo, se a rota não contem "login", exiba o menu:
-      this.showMenu = !this.router.url.includes('/login');
+      if (event instanceof NavigationEnd) {
+        this.showMenu = !this.router.url.includes('/login');
+        this.isMobileMenuOpen = false; // Fecha o menu móvel ao navegar
+      }
     });
+  }
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  deslogar(): void {
+    this.router.navigate(['/login']);
   }
 }
