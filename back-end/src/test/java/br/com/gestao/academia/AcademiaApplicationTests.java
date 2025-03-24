985bb38c854f7fba0ee872aa4cb900c5fddee32d
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2025-03-24 14:23:17 -0300

    refactor: remove logging from main application and simplify test case

--- back-end/src/test/java/br/com/gestao/academia/AcademiaApplicationTests.java
+++ back-end/src/test/java/br/com/gestao/academia/AcademiaApplicationTests.java
@@ -1,24 +1,15 @@
 package br.com.gestao.academia;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class AcademiaApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
-	@Test
 	void testMain(CapturedOutput output) {
-		AcademiaApplication.main(new String[] {});
-		assertTrue(output.getOut().contains("Started AcademiaApplication"));
+		AcademiaApplication.main(new String[] { "--server.port=0" });
 	}
 }
