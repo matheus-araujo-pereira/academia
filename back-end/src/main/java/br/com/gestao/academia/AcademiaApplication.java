0ea37aa49c991ba34451dab05b2174f9ad4f843c
Matheus Araujo Pereira <matheusaraujopereira@outlook.com>
2025-03-23 23:26:24 -0300

    test: adicionar teste para verificar a inicialização da aplicação e log de início

--- back-end/src/main/java/br/com/gestao/academia/AcademiaApplication.java
+++ back-end/src/main/java/br/com/gestao/academia/AcademiaApplication.java
@@ -2,12 +2,16 @@ package br.com.gestao.academia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class AcademiaApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(AcademiaApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaApplication.class, args);
+		logger.info("Started AcademiaApplication");
 	}
-
 }
