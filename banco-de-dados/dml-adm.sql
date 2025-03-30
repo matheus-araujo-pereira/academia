bdf9c02cf381ae86efd53bc50cf388e6c8613ecf
Matheus Araujo Pereira <matheusaraujopereira@outlook.com>
2025-03-30 11:05:39 -0300

    fix: atualizar nome do administrador no script de inserção do banco de dados

--- banco-de-dados/dml-adm.sql
+++ banco-de-dados/dml-adm.sql
@@ -1,3 +1,3 @@
 INSERT INTO academia.administrador(nome, login, senha, cpf, rg, data_nascimento, email, telefone)
-    VALUES ('Administrador da Academia', 'administrador.academia', 'admin', '06318913580', '35798769', '2001-06-19', 'matheusaraujopereira@academico.ufs.br', '79999984882');
+    VALUES ('Matheus Araujo Pereira', 'admin', 'admin', '06318913580', '35798769', '2001-06-19', 'matheusaraujopereira@academico.ufs.br', '79999984882');
 
